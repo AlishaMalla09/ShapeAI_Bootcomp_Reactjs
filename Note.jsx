4a7acf7c6013ea983e4b35572608729ba7736dff
AlishaMalla09 <85409801+AlishaMalla09@users.noreply.github.com>
2021-06-05 23:16:13 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>About JS & Paid Internship </h1>
+      <p>
+        JavaScript is the world's most popular programming language.JavaScript
+        is the programming language of the Web.JavaScript is easy to
+        learn.JavaScript is one of the 3 languages all web developers must learn
+        such as HTML to define the content of web pages.CSS to specify the
+        layout of web pages JavaScript to program the behavior of web pages
+      </p>
+    </div>
+  );
+}
+export default Note;
